9bf180c721b54f28040aa2919c9c152d0b475ecc
Akakour <akakour@qq.com>
2021-04-18 17:43:45 +0900

    mybatis分页插件

---
+++ mybatis-page/src/main/java/org/akakour/PageUtil.java
@@ -0,0 +1,29 @@
+package org.akakour;
+
+import org.akakour.bean.Page;
+
+/**
+ * @Classname PageUtil
+ * @Description TODO
+ * @Author Jack
+ * Date 2021/3/24 11:52
+ * Version 1.0
+ */
+public class PageUtil {
+
+    protected static final ThreadLocal<Page> local = new ThreadLocal<Page>();
+
+    public static <T> Page<T> enable(int pageNum,int pageSize) {
+        Page<T> page = new Page<T>(pageNum, pageSize);
+        setLocalPage(page);
+        return page;
+    }
+
+    public static <T> void setLocalPage(Page<T> page) {
+        local.set(page);
+    }
+
+    public static <T> Page<T> getLocalPage() {
+        return local.get();
+    }
+}
